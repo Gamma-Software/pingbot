8b1c4349816349879c1d1bffbfff786e7f37174a
valentin.rudloff <valentin.rudloff.perso@gmail.com>
2021-09-13 13:40:34 +0200

    fix spam when up

--- pingbot.py
+++ pingbot.py
@@ -4,8 +4,8 @@ import time
 import sys
 
 def telegram_bot_sendtext(bot_message):
-    bot_token = os.getenv('BOT_TOKEN') #'1983495325:AAH8McX0qWHbrLHmcMVlowInKX-tt8v3JAY'
-    bot_chatID = os.getenv('BOT_ID') #'1282108405'
+    bot_token = os.getenv('BOT_TOKEN')
+    bot_chatID = os.getenv('BOT_ID')
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
@@ -17,7 +17,7 @@ if __name__=="__main__":
         while True:
             hostname = sys.argv[1]
             response = os.system("ping -c 1 " + hostname)
-            if response != last_response or not last_response:
+            if response != last_response or last_response == None:
                 last_response = response
                 test = telegram_bot_sendtext(hostname + ' is up!' if response == 0 else hostname + ' is down!')
                 print("sends via telegram ", hostname + ' is up!' if response == 0 else hostname + ' is down!')
